0c3dbe0cbf49e074c3307364fc2ff4a60a9ddd94
AnnaW21 <122051561+AnnaW21@users.noreply.github.com>
2025-07-24 07:02:38 +0300

    Update telegram_bot.py

--- telegram_bot.py
+++ telegram_bot.py
@@ -3,7 +3,7 @@ from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, Callb
 import requests
 import json
 
-TOKEN = 'YOUR TELGRAM TOKEN'
+TOKEN = 'YOUR TELEGRAM TOKEN'
 API_URL = "LLAMA API"s
 HEADERS = {"Content-Type": "application/json"}
 
